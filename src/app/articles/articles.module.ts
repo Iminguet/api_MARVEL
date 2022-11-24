56144895664648dd29f2b08519cbf2c17634ced3
Iminguet <i.minguet@hotmail.com>
2022-11-24 17:30:21 +0100

    pruebas lectura de html desestructurado

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { ArticleComponent } from './article/article.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ArticleComponent, DetalleComponent],
+  imports: [CommonModule, HttpClientModule],
 })
-export class ArticlesModule { }
+export class ArticlesModule {}
---
+++ src/app/articles/detalle/detalle.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetalleComponent } from './detalle.component';
+
+describe('DetalleComponent', () => {
+  let component: DetalleComponent;
+  let fixture: ComponentFixture<DetalleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DetalleComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DetalleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
