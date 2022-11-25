8f0cc518bc79098c95621ceb829a9bb6b068a1b8
Mikiexe <matias06112001@gmail.com>
2022-11-25 11:39:30 +0100

    Resolucion de conflictos
    correccion de importes en app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlesModule } from './articles/articles.module';
 
 @NgModule({
   declarations: [AppComponent],
