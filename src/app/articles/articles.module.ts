268c152f06fa16952dbb85a45a6863ae539e26ac
Iminguet <i.minguet@hotmail.com>
2022-11-25 09:46:50 +0100

    Modulos, componentes

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -3,9 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ArticleComponent } from './article/article.component';
 import { DetalleComponent } from './detalle/detalle.component';
+import { CharactersService } from './characters.service';
+import { CharactersComponent } from './characters/characters.component';
+import { ComicsComponent } from './comics/comics.component';
+import { CreatorsComponent } from './creators/creators.component';
 
 @NgModule({
-  declarations: [ArticleComponent, DetalleComponent],
+  declarations: [
+    ArticleComponent,
+    DetalleComponent,
+    CharactersComponent,
+    ComicsComponent,
+    CreatorsComponent,
+  ],
   imports: [CommonModule, HttpClientModule],
+  providers: [CharactersService],
+  exports: [CharactersComponent],
 })
 export class ArticlesModule {}
---
+++ src/app/articles/characters/characters.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CharactersService } from '../characters.service';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.scss'],
+})
+export class CharactersComponent {
+  constructor(private characterData: CharactersService) {}
+
+  characterArray: any = null;
+
+  ngOnInit() {
+    this.characterData
+      .returnCharacter()
+      .subscribe((character) => (this.characterArray = character));
+  }
+}
