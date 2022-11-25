5a5b54e56edaa31ecd0f49ddf118914efef4caee
Iminguet <i.minguet@hotmail.com>
2022-11-25 10:48:04 +0100

    plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { ArticlesModule } from './articles/articles.module';
 
 @NgModule({
   declarations: [AppComponent],
-<<<<<<< HEAD
+
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, ArticlesModule],
-=======
-  imports: [BrowserModule, AppRoutingModule],
->>>>>>> main
+
   providers: [],
   bootstrap: [AppComponent],
 })
