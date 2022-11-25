fcbc477ce5b33a1be9d065c99b3ae73cb948c0a0
Mikiexe <matias06112001@gmail.com>
2022-11-25 10:01:28 +0100

    Incorporación de TSLib, assets de Marvel y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+import { LoginModule } from './login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', component: LoginFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
