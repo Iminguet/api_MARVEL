5ec358fe8751ba67813354d20206d14f40d323b6
Mikiexe <matias06112001@gmail.com>
2022-11-29 23:32:35 +0100

    Retocado de la maquetación dela app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'api_MARVEL';
+  declare $: any;
+
+  ngOnInit() {}
 }
 
 // TODO implementar funcion en el boton busqueda quye guarde el string y lo pase como parametro a una función que recoja la url como la del service. Habrá que crear el método dinámico
