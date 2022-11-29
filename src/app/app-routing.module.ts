12e0b54393a019977f7376e59ee57cfa47c9d0ac
Cristian <crs1996ra@gmail.com>
2022-11-29 13:57:27 +0100

    componente de about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './login/login-form/login-form.component';
 import { RegisterFormComponent } from './register/register-form/register-form.component';
-
+import { AboutusComponent } from './aboutus/aboutus/aboutus.component';
 const routes: Routes = [
   { path: 'login', component: LoginFormComponent },
   { path: 'register', component: RegisterFormComponent },
+  { path: "about", component: AboutusComponent}
 ];
 
 @NgModule({
